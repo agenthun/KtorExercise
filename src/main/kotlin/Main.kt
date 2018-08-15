9adb28773cab1489db39bbe97478323d50e14a69
agenthun <hun333@126.com>
2018-08-16 00:49:17 +0800

    [ex1] hello world

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") { call.respondText("hello word", ContentType.Text.Plain) }
+            get("/demo") { call.respond("HELLO WORD") }
+        }
+    }
+    server.start(wait = true)
+}
